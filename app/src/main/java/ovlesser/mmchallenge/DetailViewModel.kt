ecaeb55a5c9e15a24bb53d4ad2f2dc62cd8bd854
kxie <kxie@switch.tv>
2019-06-24 20:45:41 +1000

    add the calculation of PMT minor adjustment of the layout

--- app/src/main/java/ovlesser/mmchallenge/DetailViewModel.kt
+++ app/src/main/java/ovlesser/mmchallenge/DetailViewModel.kt
@@ -1,13 +1,17 @@
 package ovlesser.mmchallenge
 
 import android.os.Parcelable
-import androidx.databinding.BaseObservable
-import androidx.databinding.ObservableField
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 class DetailViewModel(
-    var money: Int = 3000,
-    var time: Int = 4
-) : BaseObservable(), Parcelable {
+    var amount: Int = 3000,
+    var month: Int = 4) : Parcelable {
+
+    private val rate = 0.1581 // guessed with the example in the description, amount=5000, month=24, weekly payment=56.15
+    fun pmt() : Double{
+        val v = 1 + rate / 52.0
+        val t = -(month.toDouble() / 12.0) * 52.0
+        return if (rate.equals(0.0)) -amount / t else amount * (rate / 52.0) / (1.0 - Math.pow(v, t))
+    }
 }
